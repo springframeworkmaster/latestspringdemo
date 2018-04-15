9dec6a9418863ed5b902bb71fad15d187fc9b2f2
Sai Krishna <sskrishnas@gmail.com>
2018-04-15 14:09:11 -0500

    Initial Commit

---
+++ src/main/java/com/springframeworkmaster/app/springdemo/HomeController.java
@@ -0,0 +1,13 @@
+package com.springframeworkmaster.app.springdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "Welcome to Cloud Foundry";
+    }
+}
