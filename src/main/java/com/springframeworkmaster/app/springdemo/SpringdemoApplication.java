1d1221c39aff1c5ceab1bfebc45c31228482fb54
Sai Krishna <30296848+springframeworkmaster@users.noreply.github.com>
2019-07-13 12:57:02 -0500

    Update SpringdemoApplication.java

--- src/main/java/com/springframeworkmaster/app/springdemo/SpringdemoApplication.java
+++ src/main/java/com/springframeworkmaster/app/springdemo/SpringdemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringdemoApplication {
 
     public static void main(String[] args) {
+        
         SpringApplication.run(SpringdemoApplication.class, args);
     }
 }
